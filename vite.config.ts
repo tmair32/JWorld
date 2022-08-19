bea3481f9149af1aea1f8a0db86f84a89e2a181c
tmair32 <tmair32@gmail.com>
2022-08-19 14:04:45 +0900

    :bug: Fixup Base Route

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ import toc from "markdown-it-table-of-contents";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   build: {
     rollupOptions: {
       input: {
