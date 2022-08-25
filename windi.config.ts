492e664f5766061d3941274b227774b3df4565fa
tmair32 <tmair32@gmail.com>
2022-08-25 16:12:33 +0900

    :sparkles: Add Resume Background with twinkle stars

--- windi.config.ts
+++ windi.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite-plugin-windicss";
 import plugin from "windicss/plugin";
+import colors from "windicss/colors";
 
 export default defineConfig({
   extract: {
     include: ["index.html", "src/**/*.{vue, ts}"],
   },
   plugins: [require("@windicss/plugin-question-mark")],
+  theme: {
+    extend: {
+      colors: {
+        "star-color-A": colors.white,
+        "star-color-B": colors.transparent,
+      },
+    },
+  },
 });
