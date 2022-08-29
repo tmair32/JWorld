322552500ce9b7607f3c8d0e5078f819d12802ba
tmair32 <tmair32@gmail.com>
2022-08-29 16:47:14 +0900

    :sparkles: Meteor Tracking Svg Path (...ing)

--- src/pages/resume/index.vue
+++ src/pages/resume/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import anime from "animejs";
+
 const scrollDirection = ref(0); // 1: up, -1: down
 const waiting = ref(false); // Throttle Flag
 const scrollTrace = ref(0); // Previous Scroll Direction
@@ -17,14 +19,35 @@ const scrollHandler = (e: WheelEvent) => {
 const clientWidth = ref(0);
 const clientHeight = ref(0);
 
-onMounted(() => {
+const path = anime.path("path");
+
+onMounted(async () => {
   document.addEventListener("mousewheel", scrollHandler);
+
+  if (typeof window === "undefined") return;
+  const ani = await import("animejs/lib/anime.es.js");
+  const anime = ani.default;
+  const path = anime.path(".meteor-path path");
+  console.log(path("x"));
+  console.log(path("y"));
+
+  anime({
+    targets: ".meteor-path .meteor",
+    translateX: path("x"),
+    translateY: path("y"),
+    // rotate: path("angle"),
+    easing: "linear",
+    duration: 12000,
+    loop: true,
+  });
 });
 </script>
 <template>
   <background />
-  <meteor />
-  <meteor-path />
+  <div class="meteor-path">
+    <meteor-path />
+    <meteor />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
